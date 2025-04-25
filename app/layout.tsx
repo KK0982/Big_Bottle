27ebea57e8e3bfc5ae5ad6ee6db99473a7324945
qwer951123 <qiuhao951@gmail.com>
2025-04-26 03:01:09 +0800

    feat: add to mobile desktop

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { VeChainProvider } from "./providers/vechain/provider";
 import { ChakraProviders } from "./providers/chakra/provider";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { ServiceWorkerRegistration } from "./components/ServiceWorkerRegistration";
 
 const roboto = Roboto({
   weight: ["400", "500", "700"],
@@ -13,11 +14,20 @@ const roboto = Roboto({
 export const metadata: Metadata = {
   title: "BigBottle",
   description: "BigBottle",
+  manifest: "/manifest.json",
+  themeColor: "#000000",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "BigBottle",
+  },
 };
 
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   viewportFit: "cover",
 };
 
@@ -27,10 +37,36 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="zh">
+    <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link
+          rel="apple-touch-startup-image"
+          media="screen and (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3)"
+          href="/splash/iPhone_14_Pro_Max_portrait.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="screen and (device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3)"
+          href="/splash/iPhone_14_Pro_portrait.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="screen and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3)"
+          href="/splash/iPhone_14_Plus__iPhone_13_Pro_Max__iPhone_12_Pro_Max_portrait.png"
+        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+      </head>
       <body className={`${roboto.className}`}>
         <ChakraProviders>
-          <VeChainProvider>{children}</VeChainProvider>
+          <VeChainProvider>
+            {children}
+            <ServiceWorkerRegistration />
+          </VeChainProvider>
         </ChakraProviders>
         <SpeedInsights />
       </body>
